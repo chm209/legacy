4211b7edd6256b08747479f43afd1805eb10bd28
chm209 <changmin7537@gmail.com>
2023-12-30 00:12:44 +0900

    [setting] MyBatisでmapperとxmlの練習

--- setting_environment/src/test/java/persistence/DataSourceTests.java
+++ setting_environment/src/test/java/persistence/DataSourceTests.java
@@ -16,7 +16,6 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import lombok.Setter;
 import lombok.extern.log4j.Log4j;
-import sample.SampleTests;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
