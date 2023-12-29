506abc7a71fb31b2e83f11e88ecce3a50cb3be79
chm209 <changmin7537@gmail.com>
2023-12-30 00:35:30 +0900

    [setting] log4j2の設定とテストコード作成
    * 残りのファイルも含む

--- setting_environment/src/main/java/sample/SampleHotel1.java
+++ setting_environment/src/main/java/sample/SampleHotel1.java
@@ -2,7 +2,6 @@ package sample;
 
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
